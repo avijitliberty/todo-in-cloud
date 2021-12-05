33b3a61ea10d36d64af138ebc5327183404f8975
avijitliberty <avijit.liberty@gmail.com>
2021-12-05 01:05:10 -0800

    Added Config Server and Config Client

---
+++ config-server/src/test/java/com/example/demo/OutOfContainerTest.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * Out-of-container test for the config server.
+ * Verifies that the server serves up configuration when asked.
+ * Uses "native" profile to obtain properties from local file system rather than GitHub.
+ * 
+ */
+
+@SpringBootTest(classes = ConfigServerApplication.class)
+@WebAppConfiguration
+@ActiveProfiles("native")	//	"native" means use local classpath location rather than GitHub.
+public class OutOfContainerTest {
+
+	@Autowired WebApplicationContext spring;
+	MockMvc mockMvc;
+	
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(spring).build();
+	}
+	
+	@Test
+	public void propertyLoadTest() throws Exception {
+
+		//	To test if this config server is working, we will simulate a "testConfig" client
+		//	calling to get properties for its default profile.  These configuration files 
+		//	(application.yml and testConfig.yml) are on the classpath as the server is 
+		//	running the 'native' profile:
+		MvcResult result =
+			mockMvc.perform(get("/testConfig-default.properties"))
+			.andExpect(status().isOk())
+			.andReturn() 
+			;
+		
+		String returned = result.getResponse().getContentAsString();
+		//	Check that the test values from the yml are present in the properties:
+		assertTrue(returned.contains("fromApplication:"));
+		assertTrue(returned.contains("applicationValue"));
+		assertTrue(returned.contains("fromTestConfig:"));
+		assertTrue(returned.contains("testConfigValue"));
+	}
+}
---
+++ todo-server/src/main/java/com/example/demo/TodoServerApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TodoServerApplication {
+
+	@Value("${todo-config}")
+	String todoConfig;
+	
+	@Value("${service.instance.name}")
+	private String instance;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoServerApplication.class, args);
+	}
+
+	@GetMapping("/todo-config")
+	public String showLuckyWord() {
+		return "The todoConfig is: " + todoConfig;
+	}
+	
+	@RequestMapping("/")
+	public String message() {
+		return "Hello from " + instance;
+	}
+
+}
